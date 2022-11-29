2dcbfc917806d52da0965655de9d7b57a1be655e
davidhieber <david.hieber@uni-tuebingen.de>
2022-11-29 16:02:28 +0100

    added simulated clinical visit base struckture

---
+++ tte_to_clinical_visit/main.py
@@ -0,0 +1,26 @@
+
+def simulated_clinical_visit_from_tte():
+    """
+    This will take infromation from the redcap data from time to event mesures and chreate simulated clinical visit data.
+    The data will be saved in a csv file.
+    :return:
+    """
+
+    # 1 get redcap export
+
+    # 2 create a opject for every patient
+
+    # 3 for every pationet get the alrady existing clinical visits
+
+    # 4 get all relevant dates for clinical visits
+
+    # 5 check what clinical visit is missing
+
+    # 6 simulate the missing clinical visits
+
+    # 7 same the Patients wih the all the clinical visits to a csv file
+
+
+
+if __name__ == '__main__':
+    simulated_clinical_visit_from_tte()
